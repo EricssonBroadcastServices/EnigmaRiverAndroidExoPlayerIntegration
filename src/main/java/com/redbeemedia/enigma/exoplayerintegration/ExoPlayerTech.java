5fad14f25c14cd6cb32df8f6fe02bd829f898f0a
Mattias Selin <mattias.selin@eyevinn.se>
2018-11-23 08:56:21 +0100

    Beginning exo player integration

---
+++ src/main/java/com/redbeemedia/enigma/exoplayerintegration/ExoPlayerTech.java
@@ -0,0 +1,99 @@
+package com.redbeemedia.enigma.exoplayerintegration;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.MediaSourceEventListener;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.source.smoothstreaming.DefaultSsChunkSource;
+import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+import com.redbeemedia.enigma.core.IPlayerImplementation;
+
+public class ExoPlayerTech implements IPlayerImplementation {
+    private final DataSource.Factory manifestDataSourceFactory;
+    private final DataSource.Factory mediaDataSourceFactory;
+    private SimpleExoPlayer player;
+    private Handler mainHandler = new Handler(Looper.getMainLooper());
+
+    public ExoPlayerTech(Context context, String appName) {
+        manifestDataSourceFactory =
+                new DefaultDataSourceFactory(
+                        context, Util.getUserAgent(context, appName));
+        mediaDataSourceFactory =
+                new DefaultDataSourceFactory(
+                        context,
+                        Util.getUserAgent(context, appName),
+                        new DefaultBandwidthMeter());
+
+        TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(new DefaultBandwidthMeter()));
+
+        this.player = ExoPlayerFactory.newSimpleInstance(context, trackSelector);
+    }
+
+    @Override
+    public void startPlayback(String url) {
+        final MediaSource mediaSource = buildMediaSource(Uri.parse(url));
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                player.prepare(mediaSource, true, false);
+                player.setPlayWhenReady(true);
+            }
+        });
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return buildMediaSource(uri, null, null);
+    }
+
+    public void attachView(View view) {
+        if(view instanceof PlayerView) {
+            ((PlayerView) view).setPlayer(player);
+        } else {
+            throw new IllegalArgumentException("Attaching view of type "+view.getClass().getName()+" is not yet supported.");
+        }
+    }
+
+    private MediaSource buildMediaSource(
+            Uri uri, @Nullable Handler handler, @Nullable MediaSourceEventListener listener) {
+        @C.ContentType int type = Util.inferContentType(uri);
+        switch (type) {
+            case C.TYPE_DASH:
+                return new DashMediaSource.Factory(
+                        new DefaultDashChunkSource.Factory(mediaDataSourceFactory),
+                        manifestDataSourceFactory)
+                        .createMediaSource(uri, handler, listener);
+            case C.TYPE_SS:
+                return new SsMediaSource.Factory(
+                        new DefaultSsChunkSource.Factory(mediaDataSourceFactory), manifestDataSourceFactory)
+                        .createMediaSource(uri, handler, listener);
+            case C.TYPE_HLS:
+                return new HlsMediaSource.Factory(mediaDataSourceFactory)
+                        .createMediaSource(uri, handler, listener);
+            case C.TYPE_OTHER:
+                return new ExtractorMediaSource.Factory(mediaDataSourceFactory)
+                        .createMediaSource(uri, handler, listener);
+            default:
+                throw new IllegalStateException("Unsupported type: " + type);
+        }
+    }
+}
