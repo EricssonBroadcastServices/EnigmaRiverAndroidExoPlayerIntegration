0f282b4c19c28bc77c8be014aaf6a28163a89cd9
Mattias Selin <mattias.selin@eyevinn.se>
2019-03-05 15:28:43 +0100

    Added stacktrace from ExoPlayerException

--- src/main/java/com/redbeemedia/enigma/exoplayerintegration/error/ExoPlayerError.java
+++ src/main/java/com/redbeemedia/enigma/exoplayerintegration/error/ExoPlayerError.java
@@ -3,6 +3,9 @@ package com.redbeemedia.enigma.exoplayerintegration.error;
 import com.google.android.exoplayer2.ExoPlaybackException;
 import com.redbeemedia.enigma.core.error.PlayerImplementationError;
 
+import java.io.IOException;
+import java.io.Writer;
+
 public class ExoPlayerError extends PlayerImplementationError  {
     private static final String INTERNAL_ERROR_CODE_FIELD_NAME = "ExoPlayerErrorCode";
 
@@ -19,5 +22,11 @@ public class ExoPlayerError extends PlayerImplementationError  {
 
     private static int internalCode(ExoPlaybackException e) {
         return InternalExoPlayerErrorCodes.getInternalErrorCode(e,1);
+    }
+
+    @Override
+    public void writeTrace(Writer writer) throws IOException {
+        super.writeTrace(writer);
+        addExceptionStackTrace(writer, exoPlaybackException);
     }
 }
