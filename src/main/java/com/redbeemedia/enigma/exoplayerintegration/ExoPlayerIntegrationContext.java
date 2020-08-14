23eb0a2953a6471526522101ed1c686bfb0ae286
empjenk <empjenk@ericsson.com>
2020-08-14 09:59:46 +0000

    r3.1.1-BETA-1

--- src/main/java/com/redbeemedia/enigma/exoplayerintegration/ExoPlayerIntegrationContext.java
+++ src/main/java/com/redbeemedia/enigma/exoplayerintegration/ExoPlayerIntegrationContext.java
@@ -1,22 +1,7 @@
 package com.redbeemedia.enigma.exoplayerintegration;
 
-import android.app.Application;
-
-import com.google.android.exoplayer2.database.ExoDatabaseProvider;
-import com.google.android.exoplayer2.offline.DefaultDownloadIndex;
-import com.google.android.exoplayer2.offline.DefaultDownloaderFactory;
-import com.google.android.exoplayer2.offline.DownloadManager;
-import com.google.android.exoplayer2.offline.DownloaderConstructorHelper;
-import com.google.android.exoplayer2.offline.WritableDownloadIndex;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
-import com.google.android.exoplayer2.upstream.HttpDataSource;
-import com.google.android.exoplayer2.upstream.cache.Cache;
-import com.google.android.exoplayer2.upstream.cache.NoOpCacheEvictor;
-import com.google.android.exoplayer2.upstream.cache.SimpleCache;
 import com.redbeemedia.enigma.core.context.EnigmaRiverContext;
 import com.redbeemedia.enigma.core.context.IModuleContextInitialization;
-
-import java.io.File;
 
 public class ExoPlayerIntegrationContext {
     private static volatile InitializedContext initializedContext = null;
@@ -34,16 +19,6 @@ public class ExoPlayerIntegrationContext {
         }
     }
 
-    public static DownloadManager getDownloadManager() {
-        assertInitialized();
-        return initializedContext.downloadManager;
-    }
-
-    public static Cache getDownloadCache() {
-        assertInitialized();
-        return initializedContext.downloadCache;
-    }
-
     public static synchronized void assertInitialized() {
         if(initializedContext == null) {
             // If EnigmaRiverContext is not yet initialized,
@@ -58,29 +33,7 @@ public class ExoPlayerIntegrationContext {
 
 
     private static class InitializedContext {
-        private final DownloadManager downloadManager;
-        private final Cache downloadCache;
-
         public InitializedContext(IModuleContextInitialization initialization) {
-            Application application = initialization.getApplication();
-
-            ExoDatabaseProvider databaseProvider = new ExoDatabaseProvider(application);
-            WritableDownloadIndex downloadIndex = new DefaultDownloadIndex(databaseProvider);
-
-            File downloadDirectory = application.getFilesDir();
-
-            File downloadContentDirectory = new File(downloadDirectory, "downloads");
-            downloadCache =
-                    new SimpleCache(downloadContentDirectory, new NoOpCacheEvictor(), databaseProvider);
-
-            HttpDataSource.Factory httpDataSourceFactory = new DefaultHttpDataSourceFactory("enigma_river_download");
-
-        DownloaderConstructorHelper downloaderConstructorHelper = new DownloaderConstructorHelper(downloadCache, httpDataSourceFactory);
-        downloadManager = new DownloadManager(
-                                            application,
-                                            downloadIndex,
-                                            new DefaultDownloaderFactory(downloaderConstructorHelper));
-
         }
     }
 }
