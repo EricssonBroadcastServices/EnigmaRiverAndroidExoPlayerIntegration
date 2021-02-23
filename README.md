29ea0031b4433f2ceeb2326f6774128e29a2e3e4
empjenk <empjenk@ericsson.com>
2021-02-23 12:19:41 +0000

    r3.1.12-BETA-USING-DRIFT-METER-LOG-OUTPUT-2

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Exo Player Integration
 
-The exo player integration library, version r3.1.12-BETA-USING-DRIFT-METER-LOG-OUTPUT.
+The exo player integration library, version r3.1.12-BETA-USING-DRIFT-METER-LOG-OUTPUT-2.
--- src/main/java/com/redbeemedia/enigma/exoplayerintegration/DriftMeter.java
+++ src/main/java/com/redbeemedia/enigma/exoplayerintegration/DriftMeter.java
@@ -142,10 +142,15 @@ import com.redbeemedia.enigma.exoplayerintegration.drift.ISpeedHandler;
                 long driftMs = DriftMeter.this.getDrift();
                 Duration drift = Duration.millis(driftMs);
                 listeners.onDriftUpdated(speed -> {
-                    Log.d("DRIFT UPDATED", "Drift updated with drift (ms): "+ driftMs);
+                    if(driftMs > 200) {
+                        Log.d("DRIFT UPDATED", "Drift updated with drift (ms): "+ driftMs);
+                    }
                     if(!exoPlayerReleased && OpenContainerUtil.getValueSynchronized(hasPlaybackSession)) {
                         PlaybackParameters currentPlaybackParameters = player.getPlaybackParameters();
-                        Log.d("DRIFT UPDATED", "Drift speed: " + speed + " pitch: " + currentPlaybackParameters.pitch);
+                        if(driftMs > 200) {
+                            Log.d("DRIFT UPDATED", "Drift speed: " + speed + " pitch: " + currentPlaybackParameters.pitch);
+                        }
+
                         player.setPlaybackParameters(new PlaybackParameters(speed, currentPlaybackParameters.pitch, currentPlaybackParameters.skipSilence));
                     }
                 }, drift);
